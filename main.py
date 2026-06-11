ed33f193861be702aa898d36cb8797815497b968
rohankumar06198-commits <rohankumar06198@gmail.com>
2026-06-11 17:23:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
  from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.button import Button
-from kivy.uix.image import Image
-from kivy.uix.label import Label
-from kivy.clock import Clock
-from kivy.core.audio import SoundLoader
-from kivy.graphics import Color, Ellipse
+...
 import random
+CricketApp().run()
