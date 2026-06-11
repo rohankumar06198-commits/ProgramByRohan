d2946557e94decce99ae76d133e11b2926408184
rohankumar06198-commits <rohankumar06198@gmail.com>
2026-06-11 17:17:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,9 @@
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+ from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
-from datetime import datetime
-
-class ProgramByRohan(App):
-    def build(self):
-        layout = BoxLayout(orientation='vertical')
-
-        layout.add_widget(Label(text="Program by Rohan"))
-
-        self.password = TextInput(password=True)
-        layout.add_widget(self.password)
-
-        btn = Button(text="Unlock")
-        btn.bind(on_press=self.unlock)
-        layout.add_widget(btn)
-
-        self.result = Label(text="")
-        layout.add_widget(self.result)
-
-        return layout
-
-    def unlock(self, instance):
-        if self.password.text == "rohank1234":
-            self.result.text = "Welcome Rohan"
-        else:
-            self.result.text = "Wrong Password"
-
-ProgramByRohan().run()
+from kivy.uix.image import Image
+from kivy.uix.label import Label
+from kivy.clock import Clock
+from kivy.core.audio import SoundLoader
+from kivy.graphics import Color, Ellipse
+import random
